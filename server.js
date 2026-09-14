79e8a894eb4c2b82f7118c9b1d7a391d3887f137
wellington-barreto <wellingtonbrt@gmail.com>
2026-09-14 17:28:47 -0300

    Penalty Shootout v5

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.glb')) {
+      res.set('Content-Type', 'model/gltf-binary');
+      res.set('Cache-Control', 'public, max-age=86400');
+    }
+  }
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Penalty Shootout running on port ${PORT}`);
+});
